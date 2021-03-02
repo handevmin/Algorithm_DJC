758c708ec928283771392fc6cb3531e164cda5fc
handevmin <handevmin@gmail.com>
2021-03-02 23:57:26 +0900

    Add week6 task

--- week6/9372_상근이의 여행.py
+++ week6/9372_상근이의 여행.py
@@ -1,10 +1,16 @@
 import sys
 
-nation, airplane = map(int, sys.stdin.readline().split())
-
-node = []
-for i in range(airplane):
-    node.append(list(map(int, sys.stdin.readline().split())))
-
-print(node)
-
+T = int(sys.stdin.readline())
+for k in range(T):
+    nation, airplane = map(int, sys.stdin.readline().split())
+    count =0
+    node = []
+    for i in range(airplane):
+        a, b = map(int, sys.stdin.readline().split())
+        if a not in node or b not in node:
+            count +=1
+            if a not in node:
+                node.append(a)
+            if b not in node:
+                node.append(b)
+    print(count)
